ff23aaedca4074768ce5271a71401259c8dbd8c4
Prakhar Agarwal <prakharagarwal19@gmail.com>
2018-12-22 21:30:00 +0530

    Added ability to generate Excel files

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var image = document.createElement('img');
 
 function loadImage(image_file) {
     fr = new FileReader();
-    fr.onload = function() {document.getElementById('image-display').src = image.src = fr.result;}
+    fr.onload = function() {document.getElementById('image-display').src = image.src = fr.result;};
     fr.readAsDataURL(image_file);
 }
 
@@ -25,14 +25,32 @@ function startConversion() {
 
 
 function convert(image_data) {
-    image_pixels = image_data.data;
+    var rgb = [];
 
-    console.log(rgbaToRgb([255, 255, 255, 1]));
+    var excel = $JExcel.new();
+    excel.set(0, undefined, undefined, undefined, excel.addStyle({ border: 'none, none, none, none' }));
+    excel.set(0, undefined, undefined, 4);
+    
+    for(var i = 0; i < image.height; i++) {
+        for(var j = 0; j < image.width; j++) {
+            index = (i * image.width) + j;
+
+            pixel_color = [ image_data.data[4*index], image_data.data[4*index + 1], image_data.data[4*index + 2], image_data.data[4*index + 3] ];
+            rgb[index] = rgbaToRgb(pixel_color, [255, 255, 255]);
+            
+            excel.set(0, j, i*3, rgb[index][0], excel.addStyle({ fill: '#' + $JExcel.rgbToHex(rgb[index][0], 0, 0) }));
+            excel.set(0, j, i*3 + 1, rgb[index][1], excel.addStyle({ fill: '#' + $JExcel.rgbToHex(0, rgb[index][1], 0) }));
+            excel.set(0, j, i*3 + 2, rgb[index][2], excel.addStyle({ fill: '#' + $JExcel.rgbToHex(0, 0, rgb[index][2]) }));
+        }
+    }
+    console.log(rgb);
+    
+    excel.generate('Image.xlsx');
 }
 
 function rgbaToRgb(rgba, background = [255, 255, 255]) {
     var rgb = [];
-    var alpha = rgba[3];
+    var alpha = rgba[3]/255;
 
     rgb[0] = Math.round(((1 - alpha) * background[0]) + (alpha * rgba[0]));
     rgb[1] = Math.round(((1 - alpha) * background[1]) + (alpha * rgba[1]));
